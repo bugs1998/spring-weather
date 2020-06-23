b4f1bc576071c4926d07a8e58b813fd267c23f1d
liujian <1173171092@qq.com>
2020-06-23 20:01:10 +0800

    增加天气相关配置

---
+++ src/main/java/com/github/ekko/springtools/config/MassageConfig.java
@@ -0,0 +1,54 @@
+package com.github.ekko.springtools.config;
+
+/**
+ * @author 刘建
+ * @date 2020/6/23 17:22
+ */
+public class MassageConfig {
+
+    /**
+     * 腾讯云账户ID
+     */
+    public static final String SECRET_ID ="腾讯云控制台获取";
+
+    /**
+     * 腾讯云账户Key
+     */
+    public static final String SECRET_KEY = "腾讯云控制台获取";
+
+    /**
+     * 短信应用ID
+     */
+    public static final String APPID = "天气API获取";
+
+    /**
+     * 签名
+     */
+    public static final String SIGN = "腾讯云通过审核的签名";
+
+    /**
+     * 模板ID
+     */
+    public static final String TEMPLATE_ID = "腾讯云通过审核的短信模板ID";
+
+    /**
+     * 城市
+     */
+    public static String City = "此处为空即可";
+
+    /**
+     * 天气API ID
+     */
+    public static String AppID2 = "天气API控制台首页获取";
+
+    /**
+     * 天气API 密钥
+     */
+    public static String AppSecret = "天气API控制台首页获取";
+
+    /**
+     * 天气API 城市ID
+     */
+    public static String CityiID = "天气API获取";
+
+}
